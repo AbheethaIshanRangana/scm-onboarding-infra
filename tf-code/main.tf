dfbb5ad95f96676907d71823e80ebb1598254354
Abheetha Ishan Rangana <d.m.abheethaishan@gmail.com>
2023-08-29 16:04:11 +0530

    Update main.tf

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -1,1 +1,28 @@
-# main.tf
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.14.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-051f7e7f6c2f40dc1" 
+  instance_type = "t3.micro" 
+
+  subnet_id = "subnet-0123456789abcdef0" 
+
+  # Security group IDs for your instance
+  vpc_security_group_ids = ["sg-0123456789abcdef0"]
+
+  key_name = "your-key-pair" # Replace with your key pair name
+
+  tags = {
+    Name = "SCM-Web-Svr"
+  }
+}
