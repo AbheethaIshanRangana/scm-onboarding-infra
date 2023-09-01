358967c3563d558b5fef53619bc15513719303c5
Abheetha Ishan Rangana <d.m.abheethaishan@gmail.com>
2023-09-01 13:25:40 +0530

    Update main.tf

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -7,27 +7,24 @@ terraform {
   }
 }
 
-provider "aws" {
-  #region = "us-east-1"
-}
-
 resource "aws_instance" "web_instance" {
-  ami                     = "ami-053b0d53c279acc90" 
-  instance_type           = "t3.micro" 
+  ami                     = "ami-053b0d53c279acc90" # Use the AMI ID for Ubuntu 20.04 in your desired region
+  instance_type           = "t3.micro"             # Change this to your desired instance type
   subnet_id               = "subnet-8f6a9ec4" 
   vpc_security_group_ids  = ["sg-042974940cbae1ab6"]
-  key_name                = "scm"
-  #user_data               = <<EOF
-  #  #!/bin/bash
-  #  sudo apt update
-  #  sudo apt install apache2 -y
-  #  sudo systemctl enable apache2
-  #  sudo systemctl start apache2
-  #  sudo ufw allow 'Apache'
-  #  sudo systemctl status apache2
-  #EOF
-  tags                    = {
-                  Name    = "SCM-Web-Svr"
+  key_name                = "scm"   # Change this to your SSH key pair name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y apache2
+              systemctl start apache2
+              systemctl enable apache2
+              echo "<html><h1>Hello from your EC2 instance</h1></html>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "SCM-Web-Svr"
   }
 }
 
