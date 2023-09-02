0ca1fd94c8be5990a984d3b6c531ee04556ae97d
Abheetha Ishan Rangana <d.m.abheethaishan@gmail.com>
2023-09-02 17:24:20 +0530

    Update main.tf

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -27,6 +27,53 @@ resource "aws_instance" "web_instance" {
   tags = {
     Name = "SCM-Web-Svr"
   }
+
+  iam_instance_profile = ""
+}
+
+resource "aws_iam_instance_profile" "scm_ec2_profile" {
+  name = "scm_ec2_profile"
+  role = aws_iam_role.scm_role.name
+}
+
+resource "aws_iam_role" "scm_role" {
+  name = scm_role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "s3_full_access_policy" {
+  name        = "s3_full_access_policy"
+  description = "Policy for full access to Amazon S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "s3:*",
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action   = "*",
+        Effect   = "Deny",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_attachment" {
+  policy_arn = aws_iam_policy.s3_full_access_policy.arn
+  role       = aws_iam_role.scm_role.name
 }
 
 output "public_ip" {
